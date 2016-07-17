2fc89f26559ec4e6966f567d35b733bd5723b22a
klmp200 <klmp200@klmp200.net>
2016-07-18 01:38:56 +0200

    First release

---
+++ install.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+
+use 5.018;
+use autodie;
+use Cwd 'realpath';
+
+open my $vimrc, '>', "$ENV{'HOME'}/.vimrc";
+
+my $script = realpath($0);
+my $path = $script =~ s#/(\w|\.)+\z##ar;
+
+select $vimrc;
+say "let syncdir=\"$path/\"";
+say 'exec "source ".syncdir."vimrc"';
+select STDIN;
+
+close $vimrc;
+
+`git clone https://github.com/gmarik/vundle.git $ENV{'HOME'}.vim/bundle/vundle`
---
+++ test.vimrc
@@ -0,0 +1,2 @@
+let syncdir="/Users/klmp200/Documents/vim-sync/"
+exec "source ".syncdir."vimrc"
